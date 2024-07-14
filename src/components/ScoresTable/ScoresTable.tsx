abe149e389faec9e366dcfd72b95084785bbdd2c
furdzik <izabela.furdzik@gmail.com>
2024-07-14 13:11:33 +0200

    fix(app): change text

--- src/components/ScoresTable/ScoresTable.tsx
+++ src/components/ScoresTable/ScoresTable.tsx
@@ -113,7 +113,7 @@ const ScoresTable = ({ config, scores, bonuses, sum, gameStarted, options, saveS
                     <Th variant={RowVariants.Sum} playerColor={player.color}>
                       <div>
                         {sum?.[`player${player.id}`]?.all || 0}
-                        <small>({'>'}1000: {sum?.[`player${player.id}`]?.sumWithoutBonuses || 0})</small>
+                        <small>(bez bonusów: {sum?.[`player${player.id}`]?.sumWithoutBonuses || 0})</small>
                       </div>
                     </Th>
                   </tr>
